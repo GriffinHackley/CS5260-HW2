48b434bea9a8b70c7d275768947a01991e737c72
Griffin Hackley <griffinhackley@yahoo.com>
2021-03-14 15:03:00 -0600

    created dummy functions

--- consumer.py
+++ consumer.py
@@ -1,3 +1,18 @@
 import boto3
 
-print("Hello World")
+def readFile():
+    print("Reading File")
+
+def createWidget():
+    print("Widget Created")
+
+def writeFile():
+    print("Writing File")
+
+def writeToDB():
+    print("Writing to DB")
+
+readFile()
+createWidget()
+writeFile()
+writeToDB()
