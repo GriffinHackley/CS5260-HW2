bb3cd2cffbb7c3660815026b2ef9e322cab0f7aa
Griffin Hackley <griffinhackley@yahoo.com>
2021-03-15 11:25:40 -0600

    made readFile function return the JSON object from the lowest keyed file in requests bucket

--- consumer.py
+++ consumer.py
@@ -1,9 +1,27 @@
 import boto3
+import json
 
 def readFile():
-    print("Reading File")
+    #get all files from the bucket
+    s3 = boto3.resource('s3')
+    requests = s3.Bucket('usu-cs5260-hackley-requests')
+    allRequests = requests.objects.all()
+
+    #get lowest keyed object from the bucket
+    lowest = next(x for x in allRequests)
+    for obj in allRequests:
+        if lowest.key > obj.key:
+            lowest = obj
+    
+    # get information from file into JSON format
+    body = lowest.get()['Body'].read()
+    return json.loads(body)
+    
+
+
 
 def createWidget():
+    # takes a file and creates a widget object 
     print("Widget Created")
 
 def writeFile():
@@ -12,7 +30,8 @@ def writeFile():
 def writeToDB():
     print("Writing to DB")
 
-readFile()
-createWidget()
-writeFile()
-writeToDB()
+json = readFile()
+print(json)
+# createWidget()
+# writeFile()
+# writeToDB()
