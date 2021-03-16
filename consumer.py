3b1ac4338a099dae64cc599b7666d3fe3b71f0b6
Griffin Hackley <griffinhackley@yahoo.com>
2021-03-15 18:29:49 -0600

    Loops and doesnt crash
    the program now loops and doesnt crash if an empty file is passed into it

--- consumer.py
+++ consumer.py
@@ -1,12 +1,20 @@
 import boto3
 import json
 import sys
+import time
 
 def readFile(bucket):
     #get all files from the bucket
     s3 = boto3.resource('s3')
     requests = s3.Bucket(bucket)
     allRequests = requests.objects.all()
+
+    size = sum(1 for _ in requests.objects.all())
+
+    print(size)
+
+    if size == 0:
+        return 0
 
     #get lowest keyed object from the bucket
     lowest = next(x for x in allRequests)
@@ -19,7 +27,14 @@ def readFile(bucket):
 
     #delete objcet from bucket
     key = lowest.key
-    # lowest.delete()
+    lowest.delete()
+
+    if size == 10:
+        print(body)
+
+    if len(body) < 4:
+        return -1
+    
 
     return (key,json.loads(body))
 
@@ -33,14 +48,11 @@ def writeFile(key,data, bucket):
     obj = s3.Object(bucket,path)
     obj.put(Body=json.dumps(data))
 
-    print(path)
-
 def writeToDB(key, data):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('widgets')
     
     table.put_item(Item=data)
-
 
 
 # syntax for writing to bucket:
@@ -54,7 +66,7 @@ if len(sys.argv) <= 1:
     #if no arguments
     bucket = 'usu-cs5260-hackley-requests'
     whereTo = 'usu-cs5260-hackley-web'
-    storage = 0
+    storage = 1
 
 elif sys.argv[2] == "db":
     bucket = sys.argv[1]
@@ -65,21 +77,37 @@ elif sys.argv[2] == "bucket":
     whereTo = sys.argv[3]
     storage = 1
 
-#read the file and return the json object and key
-info = readFile(bucket)
-key = info[0]
-data = info[1]
-request = data["type"]
+keepGoing = 0
 
-#determine what kind of request the json is
-if request == "create":
-    if storage == 1:
-        writeFile(key,data, whereTo)
-    elif storage == 0:
-        writeToDB(key,data)
+while keepGoing < 10:
+    #read the file and return the json object and key
+    info = readFile(bucket)
 
-if request == "delete":
-    print("This was a delete request")
+    if info == -1:
+        print("File was empty, moving on")
 
-if request == "change":
-    print("This was a change request")
+    elif info == 0:
+        #try 10 times, after that end program
+        keepGoing = keepGoing+1
+        print("Out of files to read")
+        print("Waiting...")
+        time.sleep(.1)
+
+    else:
+        keepGoing = 0
+        key = info[0]
+        data = info[1]
+        request = data["type"]
+
+        #determine what kind of request the json is
+        if request == "create":
+            if storage == 1:
+                writeFile(key,data, whereTo)
+            elif storage == 0:
+                writeToDB(key,data)
+
+        if request == "delete":
+            print("This was a delete request")
+
+        if request == "change":
+            print("This was a change request")
